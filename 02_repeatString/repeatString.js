c14c1929913dd2c4e15c3df92814ea8fcb81aec5
Bia <94123654+Bia725@users.noreply.github.com>
2024-02-18 17:29:28 +0100

    Update repeatString.js

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,9 @@
 const repeatString = function() {
-
+  if (times < 0) return "ERROR";
+  let string = "";
+  for (let i = 0; i < times; i++) {
+    string += word;
+  }
 };
 
 // Do not edit below this line
